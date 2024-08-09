20e7229a46e2fe3e86ddb5a1c297ba66770a6b30
Nuskan <nuskannawaz123@gmail.com>
2024-08-09 21:01:18 +0530

    comit by nuskan

--- app/src/main/java/com/testing/foodmanagement/SelectLocationActivity.java
+++ app/src/main/java/com/testing/foodmanagement/SelectLocationActivity.java
@@ -90,8 +90,6 @@ public class SelectLocationActivity extends AppCompatActivity implements OnMapRe
         FindCurrentPlaceRequest placeRequest = FindCurrentPlaceRequest.newInstance(
                 Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
             //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
             //                                          int[] grantResults)
