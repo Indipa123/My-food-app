11ebefbd4b02c8e66d9a4fd7b56e31e769e4d603
Indipa <indipa2002ayomal@gmail.com>
2024-08-11 01:26:58 +0530

    Indipa Second commit

--- app/src/main/java/com/testing/foodmanagement/CategoryFoodItemsActivity.java
+++ app/src/main/java/com/testing/foodmanagement/CategoryFoodItemsActivity.java
@@ -1,14 +1,14 @@
 package com.testing.foodmanagement;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.view.View;
-import android.view.LayoutInflater;
-
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -45,15 +45,15 @@ public class CategoryFoodItemsActivity extends AppCompatActivity {
             TextView textViewCategory = foodItemView.findViewById(R.id.textViewCategory);
             TextView textViewPrice = foodItemView.findViewById(R.id.textViewPrice);
 
+            // Set food item data
             textViewName.setVisibility(View.VISIBLE);
+            textViewPrice.setVisibility(View.VISIBLE);
 
-            // Decode the byte array into a bitmap
             byte[] imageByteArray = foodItem.getImage();
             if (imageByteArray != null && imageByteArray.length > 0) {
                 Bitmap bitmap = BitmapFactory.decodeByteArray(imageByteArray, 0, imageByteArray.length);
                 foodImageView.setImageBitmap(bitmap);
             } else {
-                // Set a default image or placeholder if the imageByteArray is null or empty
                 foodImageView.setImageResource(R.drawable.noodles);
             }
 
@@ -61,7 +61,16 @@ public class CategoryFoodItemsActivity extends AppCompatActivity {
             textViewCategory.setText(foodItem.getCategory());
             textViewPrice.setText(String.valueOf(foodItem.getPrice()));
 
+
+            // Set OnClickListener to navigate to FoodItemDetailActivity
+            foodItemView.setOnClickListener(v -> {
+                Intent intent = new Intent(CategoryFoodItemsActivity.this, FoodItemDetailActivity.class);
+                intent.putExtra("foodItem", foodItem);
+                startActivity(intent);
+            });
+
             linearLayoutCategoryFoodItems.addView(foodItemView);
         }
     }
+
 }
