8b2f8f1c8724506f6d1c0ce86331ca443653fa31
Indipa <indipa2002ayomal@gmail.com>
2024-08-13 17:57:08 +0530

    8th commit Indipa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,10 +62,12 @@ dependencies {
     implementation ("androidx.exifinterface:exifinterface:1.3.0")
     implementation ("com.sun.mail:android-mail:1.6.7")
     implementation ("com.sun.mail:android-activation:1.6.7")
-    implementation ("androidx.recyclerview:recyclerview:1.2.1")
+
     implementation ("com.google.android.material:material:1.9.0")
     implementation ("com.github.bumptech.glide:glide:4.15.1")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
+
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
 
 
 
